590503d6ae588ece34edc4f9f798a121a4f52b23
Thang Duc Toan Nguyen <tdtnguyen@knox.edu>
2019-06-19 08:01:01 +0700

    Fetch Errors
    f

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label, Row, Col } from 'reactstrap';
-import { Control, LocalForm, Errors, Option } from 'react-redux-form';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
 
 function RenderDish({ dish }) {
     if (dish != null) {
         return (
             <Card key={dish.id}>
-                <CardImg width="100%" object src={dish.image} alt={dish.name}></CardImg>
+                <CardImg width="100%" object src={baseUrl + dish.image} alt={dish.name}></CardImg>
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
